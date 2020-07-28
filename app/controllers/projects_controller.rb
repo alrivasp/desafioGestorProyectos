463d2d893b1ea826d2dce03daf66255dc71d3a6e
alrivasp <alvaro.rivas.p@gmail.com>
2020-07-28 14:00:05 -0400

    Correccion de Nombre de controlador por Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ApplicationController
+class ProjectsController < ApplicationController
   http_basic_authenticate_with name:"desafiovamoscontodo", password:"XAHTJEAS23123%23", only: :dashboard
 
   def index
